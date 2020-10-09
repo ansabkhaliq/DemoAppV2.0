75f825efbf6d516875b3a8751b0ef8ad77cb59ec
Tim <tim.uniewski@gmail.com>
2020-10-09 21:05:23 +0200

    Using i:Interaction

--- DemoAppV2.2/ViewModel/ShellViewModel.cs
+++ DemoAppV2.2/ViewModel/ShellViewModel.cs
@@ -2,18 +2,44 @@
 using MahApps.Metro.IconPacks;
 using System;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace DemoAppV2._2.ViewModel
 {
     internal class ShellViewModel : ViewModelBase
-    {        
-        public ShellViewModel() 
-        {            
+    {
+        public ShellViewModel()
+        {
             // Build the menus
-            Menu.Add(new MenuItem() { Icon = new PackIconMaterial { Kind = PackIconMaterialKind.HomeOutline }, Text = "Page 1", NavigationDestination = new Uri("Views/Page1.xaml", UriKind.RelativeOrAbsolute) });
-            Menu.Add(new MenuItem() { Icon = new PackIconMaterial { Kind = PackIconMaterialKind.Map }, Text = "Page 2", NavigationDestination = new Uri("Views/Page2.xaml", UriKind.RelativeOrAbsolute)});
-            Menu.Add(new MenuItem() { Icon = new PackIconMaterial { Kind = PackIconMaterialKind.TableLarge }, Text = "Page 3", NavigationDestination = new Uri("Views/Page3.xaml", UriKind.RelativeOrAbsolute) });
-            Menu.Add(new MenuItem() { Icon = new PackIconMaterial { Kind = PackIconMaterialKind.Signal }, Text = "Page 4", NavigationDestination = new Uri("Views/Page4.xaml", UriKind.RelativeOrAbsolute)});            
+            Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconMaterial { Kind = PackIconMaterialKind.HomeOutline },
+                Text = "Page 1",
+                NavigationDestination = new Uri("Views/Page1.xaml", UriKind.RelativeOrAbsolute),
+                Command = new DelegateCommand(() => MessageBox.Show($"Shown from Item 1"))
+            });
+            Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconMaterial { Kind = PackIconMaterialKind.Map },
+                Text = "Page 2",
+                NavigationDestination = new Uri("Views/Page2.xaml", UriKind.RelativeOrAbsolute),
+                Command = new DelegateCommand(() => MessageBox.Show($"Shown from Item 2"))
+            });
+            Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconMaterial { Kind = PackIconMaterialKind.TableLarge },
+                Text = "Page 3",
+                NavigationDestination = new Uri("Views/Page3.xaml", UriKind.RelativeOrAbsolute),
+                Command = new DelegateCommand(() => MessageBox.Show($"Shown from Item 3"))
+            });
+            Menu.Add(new MenuItem()
+            {
+
+                Icon = new PackIconMaterial { Kind = PackIconMaterialKind.Signal },
+                Text = "Page 4",
+                NavigationDestination = new Uri("Views/Page4.xaml", UriKind.RelativeOrAbsolute),
+                Command = new DelegateCommand(() => MessageBox.Show($"Shown from Item 4"))
+            });
         }
     }
 }
